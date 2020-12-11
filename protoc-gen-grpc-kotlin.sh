a4f59ef02d0cedb6503a52f386e8327b724bd06e
Natthaphon Phanit <kekhuay@users.noreply.github.com>
2020-12-11 08:31:30 +0700

    update script to minimal

--- protoc-gen-grpc-kotlin.sh
+++ protoc-gen-grpc-kotlin.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-java -jar $DIR/protoc-gen-grpc-kotlin-1.0.0-jdk7.jar "$@"
+java -jar protoc-gen-grpc-kotlin-1.0.0-jdk7.jar
